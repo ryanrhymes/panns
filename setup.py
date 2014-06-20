254caa4b98a4c886f07797db215facd04d480bb4
Liang Wang <ryanrhymes@gmail.com>
2014-06-20 15:47:43 +0300

    0.1.1 tag

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='panns',
-    version='0.1',
+    version='0.1.1',
     packages=['panns',],
     description = 'Python Approximate Nearest Neighbor / Neighbour Search',
     #long_description=open('README.md').read(),
     author = 'Liang Wang',
     author_email = 'ryanrhymes@gmail.com',
     url = 'https://github.com/ryanrhymes/panns',
-    download_url = 'https://github.com/ryanrhymes/panns/archive/0.1.zip',
+    download_url = 'https://github.com/ryanrhymes/panns/archive/0.1.1.zip',
     keywords = ['k-nn', 'knn', 'nearest neighbor', 'nearest neighbor search' 'approximate nearest neighbor', 'approximate neighbor search',
                 'nearest neighbour', 'nearest neighbour search' 'approximate nearest neighbour', 'approximate neighbour search',
                 'high demensional search', 'high-dimension data', 'LSH', 'locality sensitive hashing', 'random projection'],
